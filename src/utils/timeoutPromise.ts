53d71d53a45d7f6e05d26fb8acee0de050d88fdb
Greg Willard (r3pwn) <wickett06@gmail.com>
2023-08-27 22:35:52 -0400

    fix(timeout): throw error instead of rejecting promise

--- src/utils/timeoutPromise.ts
+++ src/utils/timeoutPromise.ts
@@ -1,6 +1,10 @@
 export const timeoutPromise = <T>(promise: Promise<T>, reason: string, ms: number): Promise<T> => {
   return Promise.race([
     promise,
-    new Promise<T>((_resolve, reject) => setTimeout(() => reject(reason), ms))
+    new Promise<T>(() => {
+      setTimeout(() => {
+        throw new Error(reason);
+      }, ms)
+    })
   ]);
 }
