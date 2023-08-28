6bc82eaa4eb2aabe025b1b8d809b45a6b5c9c294
Greg Willard (r3pwn) <wickett06@gmail.com>
2023-08-28 01:12:48 -0400

    test(libpit): add tests to cover new values

--- tests/libpit/PitData.test.ts
+++ tests/libpit/PitData.test.ts
@@ -14,7 +14,7 @@ function getFileAsByteArray(filePath: string) {
   return new Uint8Array(bytes);
 }
 
-test('unpacks and re-packs a PIT file', () => {
+test('unpacks and re-packs a legacy PIT file', () => {
   const bytes = getFileAsByteArray(`${SAMPLES_DIR}/i9100-stock-sample.pit`);
   // clone the bytes array so we can compare it later
   const unpackBytes = bytes.slice();
@@ -22,6 +22,26 @@ test('unpacks and re-packs a PIT file', () => {
   const data = new PitData();
   // unpack the data, check for success
   expect(data.unpack(bytes)).toBe(true);
+
+  // re-pack the data
+  data.pack(bytes);
+
+  // ensure all bytes match up exactly
+  expect(unpackBytes.every(function(byte, index) {
+    return byte === bytes[index];
+  })).toBe(true);
+});
+
+test('unpacks and re-packs a PIT file with additional data', () => {
+  const bytes = getFileAsByteArray(`${SAMPLES_DIR}/gtexswifi-stock-sample.pit`);
+  // clone the bytes array so we can compare it later
+  const unpackBytes = bytes.slice();
+
+  const data = new PitData();
+  // unpack the data, check for success
+  expect(data.unpack(bytes)).toBe(true);
+  expect(data.fileType).toBe('COM_TAR2');
+  expect(data.pitName).toBe('SPRD8735');
 
   // re-pack the data
   data.pack(bytes);
